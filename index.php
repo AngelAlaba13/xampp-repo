4965b5cc87fa08fbfab6a4abd71b44b18f98e7d0
Angel Kyle Alaba <angelkylealaba@gmail.com>
2024-06-19 02:14:15 +0800

    added tailwind css

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-        $user = $_GET["username"];
-        $pass = $_GET["password"];
+        $user = $_POST["username"];
+        $pass = $_POST["password"];
         if ($user == "admin" && $pass == "password123"){
             echo "Welcome";
         }
@@ -15,13 +15,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Practice</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-        <form action="index.php" method="get">
+<body class="bg-purple-100 flex justify-center items-center">
+    <!-- using get and post method -->
+        <form class="" action="index.php" method="post"> 
             <p>Username: </p>
-            <input type= "text" name="username">
+            <input type= "text" name="username" placeholder="username">
             <p>Password:</p>
-            <input type="password" name="password">
+            <input type="password" name="password" placeholder="password"> <br> <br>
             <input type="submit" value="Log in">
         </form>
 
