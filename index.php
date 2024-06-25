11e034d08c2dcdcce0ac8e0ca61c56f570a2ae5b
Angel Kyle Alaba <angelkylealaba@gmail.com>
2024-06-26 00:07:59 +0800

    modified the code

--- index.php
+++ index.php
@@ -7,20 +7,30 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        
+        <label>What do you want to order?</label> <br>
+        <input name="radio_name" type="radio" value="pizza">
+        <label>pizza</label> <br>
+        <input name="radio_name" type="radio" value="burger">
+        <label>burger</label> <br>
+        <input name="radio_name" type="radio" value="fries">
+        <label>fries</label> <br>        
+        <input type="submit" value="enter">
     </form>
     
 </body>
 </html>
 
 <?php 
-    $name = $_POST["name"];
+    $order = $_POST["radio_name"];
 
-    if(empty($name)){
+
+    echo"You ordered {$order}";
+
+    if(empty($order)){
         echo"Please enter your name";
     }
     else{
-        echo"hello {$name}, welcome to php";
+        echo"hello {$order}, welcome to php";
     }
 
     
