effd3b16fef07dd2195710675d9ed5646ea82d0c
Angel Kyle Alaba <angelkylealaba@gmail.com>
2024-06-25 23:17:23 +0800

    practices

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label>Enter your name</label> <br>
+        <input type="text" name="name">
+    </form>
+    
+</body>
+</html>
+
+<?php 
+    $name = "";
+    $name = $_GET["name"];
+
+    if(empty($name)){
+        echo"Please enter your name";
+    }
+    else{
+        echo"hello {$name}, welcome to php";
+    }
+
+    
 
 ?>
