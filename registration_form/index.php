b19d2b2112609caed8afc059d15622bb33ab9834
Angel Kyle Alaba <angelkylealaba@gmail.com>
2024-06-28 12:31:03 +0800

    added a database again

--- registration_form/index.php
+++ registration_form/index.php
@@ -18,6 +18,8 @@
         <input type="submit" name="submit_button" value="register">
 
     </form>
+
+        <input type="submit" name="check_button" value="check">
 </body>
 </html>
 
@@ -25,29 +27,37 @@
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
         $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+        if(isset($_POST["submit_button"])){
 
-        if(empty($username)){
-            echo "Username field is empty";
-        }
-        elseif(empty($password)){
-            echo "Password field is empty";
-        }
-        else{
-            $hash_password = password_hash($password, PASSWORD_DEFAULT);
-            $sql = "INSERT INTO users(username, password)
-                    VALUES
-                    ('$username', '$hash_password')";
+            if(empty($username)){
+                echo "Username field is empty";
+            }
+            elseif(empty($password)){
+                echo "Password field is empty";
+            }
+            else{
+                $hash_password = password_hash($password, PASSWORD_DEFAULT);
+                $sql = "INSERT INTO users_tb(username, password)
+                        VALUES
+                        ('$username', '$hash_password')";
 
-            try{
-                    mysqli_query($conn, $sql);
-                    echo "User sucessfully registered!";
-                    echo"Your information is appended to the database";
-                }
-                catch(mysqli_sql_exception){
-                    echo "The user is not registered";
-                }
-        }   
+                try{
+                        mysqli_query($conn, $sql);
+                        echo "User sucessfully registered!";
+                    }
+                    catch(mysqli_sql_exception){
+                        echo "The user is not registered";
+                    }
+            }  
+        }      
 }
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if(isset($_POST["check_button"])){
+            echo("Hello WORLD");
+        }
+        
+    }
 
     mysqli_close($conn);
 ?>
